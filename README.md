07a424644f6ec44828ca2cc4f15b5c291ac600c5
Wilson Hou <wilshou@gmail.com>
2020-03-25 19:42:17 +1100

    added part 1 of script

--- README.md
+++ README.md
@@ -5,28 +5,60 @@ This app consists of 2 components:
 - A script for parsing HSC syllabus files into CSV format, and
 - A graphical web application for visualising and tracking traffic lights
 
-## Scripting
+## Syllabus to JSON Script
 
 The script accepts a syllabus file in text format (.txt) and parses said text file.
 
-### Planned Steps:
+#### Planned Steps:
+
+In version 1, CSV may be skipped in favour of JSON.
 
 - Copy Syllabus file into .txt document with some manual formatting
 - Convert Syllabus into CSV file
 - Read CSV file and convert into JSON for web application consumption
 
-### Format of CSV
+#### Format of CSV
 
 Chemistry syllabus used as first test.
 
 1. Big Modules Content _(e.g: Module 3: Reactive Chemistry)_
-2. Smaller Topic _(e.g: Predicting Reactions of Metals)_
-3.
+2. Smaller Topics _(e.g: Predicting Reactions of Metals)_
+3. Specific Dot Points _(e.g: conduct practical ... in:)_
+
+#### How to Mark Up A Syllabus:
+
+Use the following signs to indicate beginning and end of content types:
+
+- Big Modules Content: &&&
+- Smaller Topics: ???
+- Dot Point: \*\*\*
+
+For example:
+
+---
+
+&&&
+Module 1
+???
+Predicting Reactions of Metals:
+\*\*\*
+
+- Conduct Practical Investigations into...
+
+\*\*\*
+???
+&&&
+
+---
 
 ## Web Application
 
 Building in process - more to come.
 
-![Concept Preview](./current-concept.png)
+_Click image for a concept preview:_
 
-[Full Current Concept (Click me!)](https://drive.google.com/file/d/1Qg6NkmXbJBD62tex6Ju0b7NDtOok31_g/view?usp=sharing)
+[![Concept Preview](./current-concept.png)](https://drive.google.com/file/d/1Qg6NkmXbJBD62tex6Ju0b7NDtOok31_g/view?usp=sharing)
+
+## Known Issues
+
+- Currently script requires manual marking up of data to separate modules, sections and content. May be automated at a future date.
---
+++ syllabus-script/src/parse-syllabus.py
@@ -0,0 +1,52 @@
+# Will write syllabus dot points into a json file
+import json
+
+
+def main():
+
+    # list out addresses of all subject syllabuses to convert
+    SUBJECT_LIST = ['chemistry-stage6-syllabus-pdf']
+    MODULE_DELIM = '&&&'
+    TOPIC_DELIM = '???'
+    DP_DELIM = '***'
+
+    # for now, just write syllabus to JSON
+    for subject in SUBJECT_LIST:
+        ''' REFACTOR THIS FOR DIFFERENT SYLLABUSES! '''
+        syllabus_dict = {'modules': []}
+
+        # for each syllabus read a text file then write it as json
+        with open(f"../syllabus/{subject}.txt") as infile:
+            # split file by module and create module dict in syllabus dict
+            module_blocks = [module_block for idx, module_block in enumerate(
+                infile.read().split(MODULE_DELIM)) if idx % 2 != 0]
+
+            # construct module dict to add to syllabus dict
+            for module in module_blocks:
+                new_module = {'name': '', 'topics': []}
+                # ! how else can we do this... refactor into a function?
+                # TODO: REFACTOR THIS!!!
+                new_module['name'] = module.split('\n')[1]
+
+                # for each module, split by topic and make topic dicts to add onto module dicts
+                topic_blocks = [topic_block for idx, topic_block in enumerate(
+                    module.split(TOPIC_DELIM)) if idx % 2 != 0]
+
+                # construct topic dict to add to module dict
+                for topic in topic_blocks:
+                    new_topic = {'name': '', 'content': ''}
+                    new_topic['name'] = topic.split('\n')[1]
+
+                    new_module['topics'].append(new_topic)
+
+                # add new module dict to the syllabus dict
+                syllabus_dict['modules'].append(new_module)
+
+        # write end file as json
+        with open(f"../end-data/{subject}.json", "w") as outfile:
+            outfile.write(json.dumps(syllabus_dict))
+            # outfile.write("\n".join(topic_blocks))
+
+
+if __name__ == '__main__':
+    main()
